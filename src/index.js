d10259fc3c359bdae5279ae4c9a681205ff766f0
mdanassaifi <cscanas533@gmail.com>
2025-10-30 13:55:56 +0530

    cnct db

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { DB_NAME } from './constants';
+
+import express from "express";
+const app = express();
+
+
+
+
+
+/*
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        app.on("error", (error) => {
+            console.error("ERROR:", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR:", error);
+        throw error;
+    }
+})();
+
+*/
