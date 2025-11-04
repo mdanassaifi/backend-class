e2641356ca3d5ab7318272c604eaffe91855aa19
mdanassaifi <cscanas533@gmail.com>
2025-11-04 15:29:40 +0530

    db

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,9 +1,10 @@
-const asyncHandler = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next))
-        .catch(err)(next(err));
+const asyncHandler = (fn) => {
+    (req, res, next) => {
+        Promise.resolve(fn(req, res, next))
+            .catch(err)(next(err));
+    }
 }
-
-export {asyncHandler};
+export { asyncHandler };
 
 
 // const asyncHandler = (fn) => async(req, res, next) => {
