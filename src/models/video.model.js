ae3615677843cb9f6faf111e01f11e5fdf7da253
mdanassaifi <cscanas533@gmail.com>
2025-11-05 12:53:15 +0530

    di

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
 
 const videoSchema = new Schema(
     {
@@ -41,8 +44,9 @@ const videoSchema = new Schema(
     {
         timestamps: true
     }
-    
+
 )
 
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
