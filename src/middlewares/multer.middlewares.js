58502110e2904d38f85ecb47422d289374398df9
mdanassaifi <cscanas533@gmail.com>
2025-11-11 16:58:46 +0530

    multer

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -4,12 +4,12 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb)  {
-        cb(null, "/tmp/my-uploads");
+        cb(null, "./public/temp");
     },
     filename: function (req, file, cb)  {
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        cb(null, file.originalname + "-" + uniqueSuffix);
+        
+        cb(null, file.originalname)
     },
 });
 
-const upload = multer({ storage: storage });
+export const upload = multer({ storage: storage });
