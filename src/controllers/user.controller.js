589d9fe56b151b76a5832ad65f84346edaa032b1
mdanassaifi <cscanas533@gmail.com>
2025-11-12 15:20:13 +0530

    done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,10 @@
-import asyncHandler from "../utils/asyncHandler.js";
+import {asyncHandler} from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'User registered successfully' });
+    res.status(200).json({ 
+        message: 'User registered successfully' 
+    });
 
 
 })
