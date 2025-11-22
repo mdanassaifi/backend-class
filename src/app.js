10c668f9a9165ff784154d4ac850dacf5f847db5
mdanassaifi <cscanas533@gmail.com>
2025-11-22 13:33:20 +0530

    bcd

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ app.use(cors(
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-
 app.use(cookieParser());
 
 // routes would be added here
